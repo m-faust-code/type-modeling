3732b9a84195dd419926c5b8c5a2b3714d4ee664
Maggie Faust <mfaust@macalester.edu>
2024-10-04 21:28:21 -0500

    Complete

--- java-type-checker/java_type_checker/types.py
+++ java-type-checker/java_type_checker/types.py
@@ -167,8 +167,14 @@ class JavaNullType(JavaType):
     Null acts as though it is a subtype of all object types. However, it raises an exception for any
     attempt to look up a method.
     """
+    is_object_type = True
+
     def __init__(self):
         super().__init__("null")
+    def is_subtype_of(self, other):
+        return other.is_object_type
+    def method_named(self, method_name):
+        raise NoSuchJavaMethod("Cannot invoke method " + method_name + "() on null")
 
 
 class JavaTypeError(Exception):
